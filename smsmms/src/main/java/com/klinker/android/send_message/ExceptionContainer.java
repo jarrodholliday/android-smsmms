000e943cdd473d0a81f151f75c22c61c010ad4b5
Mikhail Korshunov <mr.ra1ph91@gmail.com>
2017-10-20 06:26:43 +0800

    Fixed exceptions delivery system

--- smsmms/src/main/java/com/klinker/android/send_message/ExceptionContainer.java
+++ smsmms/src/main/java/com/klinker/android/send_message/ExceptionContainer.java
@@ -5,36 +5,10 @@ import android.os.Parcelable;
 
 /**
  * Created by Mikhail Korshunov on 20.10.17.
+ * Container class for exceptions
  */
 
 public class ExceptionContainer implements Parcelable {
-  private Exception exception;
-
-  public ExceptionContainer(Exception exception) {
-    this.exception = exception;
-  }
-
-  private ExceptionContainer(Parcel in) {
-    try {
-      in.readException();
-    } catch (Exception e) {
-      exception = e;
-    }
-  }
-
-  public Exception getException() {
-    return exception;
-  }
-
-  @Override
-  public int describeContents() {
-    return 0;
-  }
-
-  @Override
-  public void writeToParcel(Parcel dest, int flags) {
-    dest.writeException(exception);
-  }
 
   public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
     public ExceptionContainer createFromParcel(Parcel in) {
@@ -45,5 +19,53 @@ public class ExceptionContainer implements Parcelable {
       return new ExceptionContainer[size];
     }
   };
+  private String exceptionName;
+  private String exceptionMessage;
+  private String exceptionStackTrace;
 
+  public ExceptionContainer(Exception exception) {
+    this.exceptionName = exception.getClass().getSimpleName();
+    this.exceptionMessage = exception.getMessage();
+    this.exceptionStackTrace = getStackTrace(exception);
+  }
+
+  private ExceptionContainer(Parcel in) {
+    exceptionName = in.readString();
+    exceptionMessage = in.readString();
+    exceptionStackTrace = in.readString();
+  }
+
+  private static String getStackTrace(Exception ex) {
+    StringBuilder sb = new StringBuilder(500);
+    StackTraceElement[] st = ex.getStackTrace();
+    sb.append(ex.getClass().getName()).append(": ").append(ex.getMessage()).append("\n");
+    for (StackTraceElement aSt : st) {
+      sb.append("\t at ").append(aSt.toString()).append("\n");
+    }
+    return sb.toString();
+  }
+
+  public String getExceptionName() {
+    return exceptionName;
+  }
+
+  public String getExceptionMessage() {
+    return exceptionMessage;
+  }
+
+  public String getExceptionStackTrace() {
+    return exceptionStackTrace;
+  }
+
+  @Override
+  public int describeContents() {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags) {
+    dest.writeString(exceptionName);
+    dest.writeString(exceptionMessage);
+    dest.writeString(exceptionStackTrace);
+  }
 }
